8ed3bdda07267238dc264151ab089e6a1e02bd10
Jabber Zhou <mayjabber@gmail.com>
2014-04-10 15:05:53 +0800

    添加计划任务脚本和部署说明

---
+++ tcp_monitor/check_tcp_monitor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+WORKDIR=/openimis/SysChk
+LOGFILE=$WORKDIR/log/check_tcp_monitor.log
+cd $WORKDIR/bin
+
+procNum=`ps -ef|grep perl|grep tcp_monitor.pl|grep root|grep -v grep|wc -l`
+if [ $procNum -ge 1 ]
+  then
+  date >> $LOGFILE
+  ps -ef|grep perl|grep tcp_monitor.pl|grep root|grep -v grep >>$LOGFILE
+  echo "tcp_monitor OK" >>$LOGFILE
+else
+  date >> $LOGFILE
+  ps -ef|grep perl|grep tcp_monitor.pl|grep root|grep -v grep >>$LOGFILE
+  echo "tcp_monitor Error" >>$LOGFILE
+  /usr/bin/perl $WORKDIR/bin/tcp_monitor.pl -d >>$LOGFILE
+   
+   ps -ef|grep perl|grep tcp_monitor.pl|grep root|grep -v grep >>$LOGFILE
+fi 
---
+++ tcp_monitor/restart_tcp_monitor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+WORKDIR=/openimis/SysChk
+LOGFILE=$WORKDIR/log/check_tcp_monitor.log
+cd $WORKDIR/bin
+
+  date >> $LOGFILE
+  ps -ef|grep perl|grep tcp_monitor.pl|grep root|grep -v grep >>$LOGFILE
+  echo "kill tcp_monitor" >>$LOGFILE
+  ps -ef|grep perl|grep tcp_monitor.pl|grep root|grep -v grep |awk '{print $2}' |xargs kill -9
+  echo "restart tcp_monitor begin" >>$LOGFILE
+  ps -ef|grep perl|grep tcp_monitor.pl|grep root|grep -v grep >>$LOGFILE
+  /usr/bin/perl $WORKDIR/bin/tcp_monitor.pl -d >>$LOGFILE
+ 
+  echo "restart tcp_monitor end" >>$LOGFILE
+  ps -ef|grep perl|grep tcp_monitor.pl|grep root|grep -v grep >>$LOGFILE
