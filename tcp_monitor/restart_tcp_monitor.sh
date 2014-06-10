d8fb450b3b731a11d48aab6ccd6da7c1625d5850
Jabber Zhou <mayjabber@gmail.com>
2014-06-10 13:44:39 +0800

    清理10天前的日志

--- tcp_monitor/restart_tcp_monitor.sh
+++ tcp_monitor/restart_tcp_monitor.sh
@@ -12,3 +12,11 @@ cd $WORKDIR/bin
  
   echo "restart tcp_monitor end" >>$LOGFILE
   ps -ef|grep perl|grep tcp_monitor.pl|grep root|grep -v grep >>$LOGFILE
+
+#let "oldd=$(date +%s) - 10 * 24 * 3600"
+#oldday=$(date --date="@$oldd" +"%a %b %e")
+oldday=$(perl -e "use POSIX qw(strftime); print strftime '%a %b %e' , localtime( time()-3600*24*10) ")
+let "linenum=$(grep -n -e "$oldday" $LOGFILE|head -1|cut -d: -f1) -1"
+if [[ $linenum > 0 ]];then
+    sed -e "1,${linenum}d" -i $LOGFILE
+fi
