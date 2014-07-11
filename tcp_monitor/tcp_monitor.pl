bd97a03485c04f179b4a4219fd644defc4f90e7c
Jabber Zhou <mayjabber@gmail.com>
2014-07-11 09:50:58 +0800

    modify date

--- tcp_monitor/tcp_monitor.pl
+++ tcp_monitor/tcp_monitor.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 #author:        zwj@skybility.com
 #version:       1.6.1
-#last modfiy:   2014-07-03
+#last modfiy:   2014-07-10
 #This script is tcp status from netstat and alarm when it is over threshold.
 #changelog:
 #    0.1 tcp连接状态计数监控脚本
