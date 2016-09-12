1692aeb8a0126f9b3a8cd0ae75f5758cbf987b5d
Jabber Zhou <mayjabber@gmail.com>
2016-09-12 10:52:25 +0800

    blablabla

--- getip.pl
+++ getip.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 #This script is use to get all ip address of a Suse Linux Server.
 #author: zwj
-#version: 0.2
+#version: 0.3
 #Oupput Format:
 #    host,perm,srv,float,man,other
 
@@ -27,13 +27,19 @@ if ($HA){
     if ($ipshow =~ /inet\s(.+?)\/\d+\s[\w\s.]+global\ssecondary/){
         $FIP = $1;
     }
-}
-
-if ($ipshow =~ /bond0\s+inet\s(.+?)\/\d+\s[\w\s.]+global\s/){
-    $SIP = $1;
-}
-if ($ipshow =~ /bond1\s+inet\s(.+?)\/\d+\s[\w\s.]+global\s/){
-    $PIP = $1;
+    if ($ipshow =~ /\d+\:\s(\w+)\s+inet\s.+?\/\d+\s[\w\s.]+global\ssecondary/){
+        my $SEth = $1;
+        if ($ipshow =~ /$SEth\s+inet\s(.+?)\/\d+\s[\w\s.]+global\s/){
+            $SIP = $1;
+        }
+    }
+}else{
+    if ($ipshow =~ /bond0\s+inet\s(.+?)\/\d+\s[\w\s.]+global\s/){
+        $SIP = $1;
+    }
+    if ($ipshow =~ /bond1\s+inet\s(.+?)\/\d+\s[\w\s.]+global\s/){
+        $PIP = $1;
+    }
 }
 
 
@@ -42,13 +48,18 @@ foreach my $line (@iplines) {
     if ($line =~ /^\d+\:\slo/){next;}
     if ($line =~ /inet\s(.+?)\/\d+\s[\w\s.]+/){
         my $ip = $1;
+        if (not $SIP){
+            if ($ip =~ /^10\./){
+                $SIP = $ip;
+                next;
+            }
+        }
         if (not $PIP){
             if ($ip =~ /^10\./){
                 $PIP = $ip;
                 next;
             }
         }
-#        if (not $FIP and not $HA){$FIP = $ip;next;}
         if ($ip eq $PIP or $ip eq $SIP or $ip eq $FIP){next;}
         $OIP = $OIP.",$ip";
     }
