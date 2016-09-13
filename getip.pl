f6db51c23008d983cc29a5a553bcece663e1383a
Jabber Zhou <mayjabber@gmail.com>
2016-09-13 08:39:49 +0800

    add oracle rule

--- getip.pl
+++ getip.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 #This script is use to get all ip address of a Suse Linux Server.
 #author: zwj
-#version: 0.4
+#version: 0.5
 #Oupput Format:
 #    host,perm,srv,float,man,other
 
@@ -13,9 +13,14 @@ chomp $hostname;
 my ($PIP,$SIP,$FIP,$MIP,$OIP) = ('') x 5;
 
 my $HA = -f '/opt/ha/conf/cluster.xml';
+
 my $DB2 = 1;
 if ($hostname =~ /\-/){
     $DB2 = 0;
+}
+my $ORACLE = 0;
+if ($hostname =~ /\-R\d{2}$/){
+    $ORACLE = 1;
 }
 
 my $ipshow = `ip -o addr show`;
@@ -23,7 +28,12 @@ my $ipshow = `ip -o addr show`;
 if ($DB2){
 }
 
-if ($HA){
+if($ORACLE){
+    if ($ipshow =~ /bond0\s+inet\s(.+?)\/\d+\s[\w\s.]+global\ssecondary/){
+        ($PIP, $SIP, $FIP) = ($ipshow =~ /bond0\s+inet\s(.+?)\/\d+\s[\w\s.]+global\s/g);
+        if (not $FIP){ $FIP = '';}
+    }
+}elsif ($HA){
     if ($ipshow =~ /inet\s(.+?)\/\d+\s[\w\s.]+global\ssecondary/){
         $FIP = $1;
     }
@@ -34,10 +44,10 @@ if ($HA){
         }
     }
 }else{
-    if ($ipshow =~ /bond0\s+inet\s(.+?)\/\d+\s[\w\s.]+global\s/){
+    if ($ipshow =~ /bond0\s+inet\s(10\.235\..+?)\/\d+\s[\w\s.]+global\s/){
         $SIP = $1;
     }
-    if ($ipshow =~ /bond1\s+inet\s(.+?)\/\d+\s[\w\s.]+global\s/){
+    if ($ipshow =~ /bond1\s+inet\s(10\.235\..+?)\/\d+\s[\w\s.]+global\s/){
         $PIP = $1;
     }
 }
@@ -50,13 +60,13 @@ foreach my $line (@iplines) {
         my $ip = $1;
         if ($ip eq $PIP or $ip eq $SIP or $ip eq $FIP){next;}
         if (not $SIP){
-            if ($ip =~ /^10\./){
+            if ($ip =~ /^10\.235/){
                 $SIP = $ip;
                 next;
             }
         }
         if (not $PIP){
-            if ($ip =~ /^10\./){
+            if ($ip =~ /^10\.235/){
                 $PIP = $ip;
                 next;
             }
